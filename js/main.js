263c9d14346b792ac214b4877973bbd82fb99537
naoki fujiwara <nfujiwara@hotmail.com>
2021-02-19 22:33:30 -0800

    rev html file, start js file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 // Start writing JavaScript here!
+const carousel = document.querySelector('.carousel')
+const previousButton = carousel.querySelector('.previous-button')
+const nextButton = carousel.querySelector('.next-button')
+const contents = carousel.querySelector('.carousel__contents')
+
+nextButton.addEventListener('click',e=>{
+  const currentSlide = contents.querySelector('.is-selected')
+  // console.log(currentSlide)
+  const nextSlide = currentSlide.nextElementSibling
+  // console.log(nextSlide)
+  const destination = getComputedStyle(nextSlide).left
+  console.log(destination)
+  contents.style.left = `-${destination}`
+  currentSlide.classList.remove('is-selected')
+  nextSlide.classList.add('is-selected')
+})
